e6b972f388165550a63a044ca7a78c009d7a1f6c
Mostafa Negim <mostafa.negim@gmail.com>
2021-05-25 18:24:44 +0430

    add: timecheck-with-config.yaml

--- ch07/command.sh
+++ ch07/command.sh
@@ -48,3 +48,13 @@ kubectl logs -l app=timecheck
 kubectl exec deploy/timecheck -- cat /logs/timecheck.log
 # check the config setup:
 kubectl exec deploy/timecheck -- cat /config/appsettings.json
+
+#
+# apply the ConfigMap and the new Deployment spec:
+kubectl apply -f timecheck/timecheck-configMap.yaml -f timecheck/timecheck-with-config.yaml
+# wait for the containers to start:
+kubectl wait --for=condition=ContainersReady pod -l app=timecheck,version=v2
+# check the log file in the new app container:
+kubectl exec deploy/timecheck -- cat /logs/timecheck.log
+# see the config file built by the init container:
+kubectl exec deploy/timecheck -- cat /config/appsettings.json
