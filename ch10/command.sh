611dbf771728b26267b555af5e696f512d0d5821
Mostafa Negim <mostafa.negim@gmail.com>
2021-06-06 12:16:03 +0430

    update: command.sh - set with helm

--- ch10/command.sh
+++ ch10/command.sh
@@ -5,3 +5,11 @@ helm repo add kiamol https://kiamol.net
 helm repo update
 # search for an app in the repo cache:
 helm search repo vweb --versions
+
+# inspect the default values stored in the chart:
+helm show values kiamol/vweb --version 1.0.0
+# install the chart, overriding the default values:
+helm install --set servicePort=8010 --set replicaCount=1 ch10-vweb \
+      kiamol/vweb --version 1.0.0
+# check the releases you have installed:
+helm ls
